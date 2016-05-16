221a3e3c470d0e150ef19dd6c6640adcf863b45f
Lay <veryide@qq.com>
2016-05-16 18:15:53 +0800

    菜单链接更新，增加 Form 相关文档；

--- res/static.js
+++ res/static.js
@@ -6,7 +6,7 @@
 function dump( data ){
 	var str = '';
 	for( var key in data ){
-	      str += '"'+ key +'" : "'+data[key]+'",';  
+	      str += '"'+ key +'" : "'+data[key]+'",';
 	}
 	return ( '{'+str+'}' ).replace(/,\}/g,'}');
 }
@@ -15,61 +15,48 @@ function dump( data ){
 R.reader(function(){
 
 	R("#build").html( "当前版本：" + R.version + " 最后更新：" + R.build );
-	
+
 	///////////////////////////////
-	
+
 	//当前路径，过滤掉 # 以后的内容
 	var self = location.href.replace(/#([\S\s])*$/,'');
-	
+
 	//处理导航
 	R("nav ul li a").each(function(){
-		
+
 		//高亮当前，如果在 api 目录时高亮 api.html
-		if( 
-		   this.href == self || 
+		if(
+		   this.href == self ||
 		   ( self.substring( self.length-1 , self.length ) == "/" && this.href.indexOf("index.html") > -1 ) ||
 		   ( this.href.indexOf("api.html") > -1 && self.indexOf("api/") > -1 )
 		){
 			this.className = "active";
 		}
-		
+
 	});
-	
+
 	///////////////////////////////
-	
-	//搜索处理
-	R("#search input").bind('focus',function(){
-	
-		this.value = ( this.value == this.defaultValue ) ? "" : this.value;
-		
-	}).bind('blur',function(){
-	
-		this.value = !this.value ? this.defaultValue : this.value;
-		
-	});
-	
-	///////////////////////////////
-	
+
 	//目录索引
-	R("#entry").insert( 'menu', {
+	R("#entry").before( 'menu', {
 		"id" : "index",
 		"innerHTML" : "<ul></ul>"
-	});	
-	
+	});
+
 	//创建索引
 	R("#entry dt").each(function(){
-		
+
 		//条目名称
-		var text = R.String( this.innerHTML ).trim().output();		
+		var text = R.String( this.innerHTML ).trim().output();
 		var hash = escape( text );
-		
+
 		//加入到索引
-		R("#index ul").create( 'li', {
+		R("#index ul").append( 'li', {
 			"innerHTML" : "<a href='#"+ hash +"'>"+ text +'</a>'
 		});
-		
+
 		//创建锚点
-		R( this ).create( 'a', {
+		R( this ).append( 'a', {
 			"name" : hash
 		});
 
